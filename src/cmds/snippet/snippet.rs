7fd75f7f3187ca7d77dd73f0d916264a31a24bb6
rayzhuca <raybipse@gmail.com>
2025-09-11 22:57:12 -0400

    add list and find snippet tool

--- src/cmds/snippet/snippet.rs
+++ src/cmds/snippet/snippet.rs
@@ -1,17 +1,21 @@
+use ansi_term::Colour::Red;
+use ansi_term::Style;
+
 use clap::{arg, Args};
 
-use std::process;
+use std::ffi::OsStr;
+use std::path::PathBuf;
+use std::{fs, process};
 
 #[derive(Args, Debug)]
 pub struct SnippetArgs {
     name: Option<String>,
 
-    #[arg(short, long, default_value_t = true)]
+    #[arg(short, long, default_value_t = false)]
     create: bool,
 }
 
 pub fn snippet(args: &SnippetArgs) {
-    println!("snippet tool ran! {:?}", args);
     if let Some(name) = &args.name {
         if args.create {
             create(&name);
@@ -27,12 +31,46 @@ pub fn snippet(args: &SnippetArgs) {
     }
 }
 
+fn get_store_path() -> PathBuf {
+    return PathBuf::from(&format!(
+        "{}/src/cmds/snippet/snippet_store/",
+        env!("CARGO_MANIFEST_DIR")
+    ));
+}
+
 fn list() {
-    println!("list called");
+    let entries = fs::read_dir(get_store_path().as_path()).unwrap();
+    let mut snippets = vec![];
+    for entry in entries {
+        let path = entry.unwrap().path();
+        if path.extension() != Some(OsStr::new("txt")) {
+            eprintln!(
+                "{}",
+                Red.paint(format!(
+                    "Unexpected non-text file: {}",
+                    path.to_str().unwrap()
+                ))
+            );
+            continue;
+        }
+        let file_name = path.file_stem().unwrap();
+        snippets.push(file_name.to_str().unwrap().to_string());
+    }
+    snippets.sort();
+
+    if snippets.is_empty() {
+        println!("{}", Style::new().bold().paint("No snippets found."));
+    } else {
+        println!("{}", Style::new().bold().paint("Snippets:"));
+        println!("  - {}", snippets.join("\n  - "));
+    }
 }
 
 fn find(name: &str) {
-    println!("find called {:?}", name);
+    let mut path = get_store_path().clone();
+    path.push(format!("{}.txt", name));
+    let contents = fs::read_to_string(path).unwrap();
+    print!("{}", contents);
 }
 
 fn create(name: &str) {
